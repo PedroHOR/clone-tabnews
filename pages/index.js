91c57cec54ceee1fa7562566712391cadf2fe21f
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-22 02:42:37 +0000

    nova imagem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 function Home(){
+
+    let img = document.createElement("img");
+        img.src = "https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg";
+        document.body.appendChild(img);
     return <h1>kikikikikiki</h1>
 }
 
