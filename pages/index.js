b77a3f3de6a334d98d7fce4b82888e264e93c126
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-22 02:48:42 +0000

    nova imagem final

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home(){
 
-    
     return <img id="minhaImagem" src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg" alt="Descrição da imagem"></img>
+}
+
+function Texto(){
+    
+    return <h1> Voce parece pato kikiki</h1>
 }
 
 export default Home;
