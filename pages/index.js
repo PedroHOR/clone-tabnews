b5c34858598b307575049902e37b582fbe2d1958
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-22 02:46:56 +0000

    nova imagem teste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
+
     
-    <img id="minhaImagem" src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg" alt="Descrição da imagem"></img>
-    return <h1>kikikikikiki</h1>
+    return <img id="minhaImagem" src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg" alt="Descrição da imagem"></img>
 }
 
 export default Home;
