b63e103ab3f7a6f8d602169a0e2b2dabafd6eb43
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-25 02:28:02 +0000

    Adiciona .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 function Home() {
   return (
-    <img>
+    <img
       id="minhaImagem"
       src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg"
       alt="Descrição da imagem"
-    </img>
+    ></img>
   );
 }
-
-function Texto() {
-  return <h1> Voce parece pato kikiki</h1>;
-}
-
-function teste() {
-  console.log("teste");
-}
-
 export default Home;
