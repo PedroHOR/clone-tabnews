622d4058fb269d2d28cbf7d9a62c0a5b3696f49e
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-25 02:20:08 +0000

    adiciona scripts lint check e lint fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-function Home(){
-
-    return <img id="minhaImagem" src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg" alt="Descrição da imagem"></img>
+function Home() {
+  return (
+    <img>
+      id="minhaImagem"
+      src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg"
+      alt="Descrição da imagem"
+    </img>
+  );
 }
 
-function Texto(){
-    
-    return <h1> Voce parece pato kikiki</h1>
+function Texto() {
+  return <h1> Voce parece pato kikiki</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
