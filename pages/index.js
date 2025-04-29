8045fc2ba1a6609831585fba3471e1e03e792943
PedroHOR <92756234+PedroHOR@users.noreply.github.com>
2025-04-29 02:37:00 +0000

    Atualizacao pagina inicial site

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
 function Home() {
-  return (
-    <img
-      id="minhaImagem"
-      src="https://i.ibb.co/ccTSt7kW/Screenshot-20250421-233909-Instagram.jpg"
-      alt="Descrição da imagem"
-    ></img>
-  );
+  return <h1>"O gatinho é tongo</h1>;
 }
 export default Home;
